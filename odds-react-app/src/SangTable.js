ba232937b0b6c06524d0d3e3736bbe073d8eb4ab
seoularpro <sangkyun.hanpro@gmail.com>
2024-09-08 08:27:32 -0700

    update last update message

--- odds-react-app/src/SangTable.js
+++ odds-react-app/src/SangTable.js
@@ -354,7 +354,7 @@ export default function SangTable(props) {
         ))}
       </table>
       <div class="updateTimeSection">
-        Median values last updated Saturday, 9/7 at 4:53pm ET
+        Median values last updated Sunday, 9/8 at 11:27am ET
       </div>
       <div className="new-feature-message">
         Player Selector Table
